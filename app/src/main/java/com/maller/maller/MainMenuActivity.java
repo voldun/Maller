74e258f2db205f7ad5f5bcf79caed4c647e0d26c
voldun <surkani@mail.ru>
2016-07-20 03:44:11 +0300

    test

--- app/src/main/java/com/maller/maller/MainMenuActivity.java
+++ app/src/main/java/com/maller/maller/MainMenuActivity.java
@@ -16,7 +16,7 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); 
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
